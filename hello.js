b1ec2a7e8c15a7b55c000bc9dc2a786534ddc4f9
Meghavati Chaudhari <meghapjc29@gmail.com>
2024-01-07 01:15:21 +0530

    modules learning has done

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log('js is cool')
+// console.log('js is cool')
+// const math = require("./math");
+
+// can also destructure
+const {add,sub} = require('./math');
+// console.log(add(2,3), sub(6,6))
+console.log(add(2,3))
+//  console.log(math.add(2,3),math.sub(5,6));
+// console.log(math)
